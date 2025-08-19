18e3721de1762306b7a926e893110e25d72299e5
Jaap van Otterdijk <jvanotterdijk@tilaa.com>
2025-08-19 14:03:33 +0200

    Add Terraform deployment

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    nexaa = {
+      source = "nexaa-cloud/nexaa"
+    }
+  }
+}
+
+provider "nexaa" {
+  username = var.nexaa_username
+  password = var.nexaa_password
+}
+
+resource "nexaa_namespace" "kroket" {
+  name        = "nexaa-kroket"
+}
+
+resource "nexaa_container" "container" {
+  name      = "kroket-friday"
+  namespace = nexaa_namespace.kroket.name
+  image     = "nginx:latest"
+
+  resources = {
+    cpu = 0.25
+    ram = 0.5
+  }
+
+  ports = ["80:80"]
+
+  ingresses = [
+    {
+      domain_name = null
+      port        = 80
+      tls         = true
+    }
+  ]
+
+  health_check = {
+    port = 80
+    path = "/"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "nexaa_username" {
+  description = "Username for Nexaa authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "nexaa_password" {
+  description = "Password for Nexaa authentication"
+  type        = string
+  sensitive   = true
+}
